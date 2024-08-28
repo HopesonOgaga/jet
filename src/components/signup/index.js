6e38e21fa8f64669a052b3a55048a69bea403f90
hopeson <hopesonogaga@gmail.com>
2024-08-28 21:54:04 +0100

    registration ui

--- src/components/signup/index.js
+++ src/components/signup/index.js
@@ -2,124 +2,138 @@ import React from "react";
 import Header from "../header";
 import Footer from "../footer";
 
-export default function SignUP() {
+export default function SignUp() {
   return (
     <section>
       <Header />
-      <div className="grid grid-cols-3 w-full h-full ">
-        <div className="w-full h-full">
-          <img className="w-full" src="/register.jpg" alt="register"></img>
+      <div className="grid grid-cols-2 w-full h-full gap-4 justify-evenly">
+        <div className="w-full h-full col-span-1">
+          <img className="w-full" src="/register.jpg" alt="register" />
         </div>
-        <div className="col-span-2 ">
-          <div>
-            <p>sign up</p>
-            <p>please fill your form</p>
-          </div>
-          <div>
-            <form>
+        <div className="col-span-1 w-full h-full">
+          <div className="place-items-end w-full h-full justify-end pl-6 pr-6">
+            <form className="flex flex-col gap-2 h-full">
+              <div className="flex gap-1 flex-col mb-6">
+                <p className="font-bold text-2xl capitalize">Sign Up</p>
+                <p>Please fill your form</p>
+              </div>
               <div>
                 <label
-                  for="email"
-                  class="block mb-2 text-sm font-medium text-gray-900 "
+                  htmlFor="name"
+                  className="block mb-2 font-medium text-gray-900 capitalize"
                 >
-                  Your email
+                  Full Name
                 </label>
                 <input
-                  type="email"
-                  id="email"
-                  class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
-                  placeholder="name@flowbite.com"
+                  type="text"
+                  id="name"
+                  className="border border-none border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-5/6 h-14 p-2.5 bg-gray-200 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white hover:border-orange-500 hover:ring-orange-500 hover:ring-2"
                   required
                 />
               </div>
               <div>
                 <label
-                  for="email"
-                  class="block mb-2 text-sm font-medium text-gray-900 "
+                  htmlFor="hostname"
+                  className="block mb-2 font-medium text-gray-900 capitalize"
                 >
-                  Your email
+                  Host Name
                 </label>
                 <input
-                  type="email"
-                  id="email"
-                  class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
-                  placeholder="name@flowbite.com"
+                  type="text"
+                  id="hostname"
+                  className="border border-none border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-5/6 h-14 p-2.5 bg-gray-200 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white hover:border-orange-500 hover:ring-orange-500 hover:ring-2"
                   required
                 />
               </div>
-              <section className="flex gap-6 ">
-                <div>
+              <section className="flex gap-3 w-full">
+                <div className="w-full">
                   <label
-                    for="email"
-                    class="block mb-2 text-sm font-medium text-gray-900 "
+                    htmlFor="email"
+                    className="block mb-2 font-medium text-gray-900 capitalize"
                   >
-                    Your email
+                    Email
                   </label>
                   <input
                     type="email"
                     id="email"
-                    class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+                    className="border border-none border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-5/6 h-12 p-2.5 bg-gray-200 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white hover:border-orange-500 hover:ring-orange-500 hover:ring-2"
                     placeholder="name@flowbite.com"
                     required
                   />
                 </div>
-                <div>
+                <div className="w-full">
                   <label
-                    for="email"
-                    class="block mb-2 text-sm font-medium text-gray-900 "
+                    htmlFor="phoneNumber"
+                    className="block mb-2 font-medium text-gray-900 capitalize"
                   >
-                    Your email
+                    Phone Number
                   </label>
                   <input
-                    type="email"
-                    id="email"
-                    class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
-                    placeholder="name@flowbite.com"
+                    type="text"
+                    id="phoneNumber"
+                    className="border border-none border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-5/6 h-12 p-2.5 bg-gray-200 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white hover:border-orange-500 hover:ring-orange-500 hover:ring-2"
                     required
                   />
                 </div>
               </section>
               <div>
                 <label
-                  for="email"
-                  class="block mb-2 text-sm font-medium text-gray-900 "
+                  htmlFor="password"
+                  className="block mb-2 font-medium text-gray-900 capitalize"
                 >
-                  Your email
+                  Password
                 </label>
                 <input
-                  type="email"
-                  id="email"
-                  class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
-                  placeholder="name@flowbite.com"
+                  type="password"
+                  id="password"
+                  className="border border-none border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-5/6 h-14 p-2.5 bg-gray-200 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white hover:border-orange-500 hover:ring-orange-500 hover:ring-2"
                   required
                 />
               </div>
               <div>
                 <label
-                  for="email"
-                  class="block mb-2 text-sm font-medium text-gray-900 "
+                  htmlFor="confirmPassword"
+                  className="block mb-2 font-medium text-gray-900 capitalize"
                 >
-                  Your email
+                  Confirm Password
                 </label>
                 <input
-                  type="email"
-                  id="email"
-                  class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
-                  placeholder="name@flowbite.com"
+                  type="password"
+                  id="confirmPassword"
+                  className="border border-none border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-5/6 h-14 p-2.5 bg-gray-200 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white hover:border-orange-500 hover:ring-orange-500 hover:ring-2"
                   required
                 />
               </div>
-              <section>
-                <button>create account</button>
+              <section className="flex justify-between">
+                <div>
+                  <input
+                    type="checkbox"
+                    name="newsletter"
+                    className=""
+                    value=""
+                  />
+                  <label
+                    htmlFor="newsletter"
+                    className="text-gray-300 capitalize pl-2"
+                  >
+                    Subscribe to our newsletter
+                  </label>
+                </div>
+                <button
+                  type="submit"
+                  className="hover:bg-orange-400 active:bg-orange-600 w-36 h-12 bg-orange-600 rounded-lg shadow-md text-white font-medium capitalize"
+                >
+                  Create Account
+                </button>
               </section>
               <div className="flex justify-between w-full mt-6">
-                <p>Don't have an account?</p>
+                <p>Already have an account?</p>
                 <a
                   href="#"
                   target="_blank"
                   className="text-orange-500 font-semibold capitalize"
                 >
-                  Sign up
+                  Sign in
                 </a>
               </div>
               <div className="flex gap-4 w-full h-full mt-12">
@@ -143,7 +157,11 @@ export default function SignUP() {
                 </div>
                 <div className="w-8 h-8 border-white border-2 rounded-full flex items-center justify-center">
                   <a href="#" target="_blank">
-                    <img className="w-6 h-6" src="/twitter.png" alt="Twitter" />
+                    <img
+                      className="w-6 h-6"
+                      src="/twitter.png"
+                      alt="Twitter"
+                    />
                   </a>
                 </div>
               </div>
